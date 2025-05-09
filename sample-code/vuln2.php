bdc196e99fb807f0be2adef38b54cf0bb7edaea4
DK <16559499+ZeroDayD@users.noreply.github.com>
2025-05-09 15:29:15 +0300

    Create vuln2.php

---
+++ sample-code/vuln2.php
@@ -0,0 +1,6 @@
+<?php
+ // SQLi via $_POST
+ $conn = mysqli_connect("localhost", "root", "", "test");
+ $sql = "SELECT * FROM users WHERE name = '" . $_POST['name'] . "'";
+ $result = mysqli_query($conn, $sql);
+ ?>
